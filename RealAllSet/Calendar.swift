8f907ec8889ee6bfb9a739b3295f383999b77770
Alexandra Akyuz <bananalex2009@gmail.com>
2025-07-29 14:07:36 -0700

    added help and calendar views, linked buttons

---
+++ RealAllSet/Calendar.swift
@@ -0,0 +1,22 @@
+//
+//  Calendar.swift
+//  RealAllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct Calendar: View {
+    var body: some View {
+        ZStack{
+            Color("vanilla")
+                .ignoresSafeArea()
+            Text("THIS IS THE CALENDAR VIEW")
+        }//zstack
+    }//body
+}//struct
+
+#Preview {
+    Calendar()
+}
