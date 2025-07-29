05563736eadfb551b81947fc5669eabdd724192b
Alexandra Akyuz <bananalex2009@gmail.com>
2025-07-29 12:46:40 -0700

    Initial Commit

---
+++ RealAllSet/RealAllSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealAllSetApp.swift
+//  RealAllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RealAllSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
