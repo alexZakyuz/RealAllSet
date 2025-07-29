e5f930eb2bb7867617c2138f19cf70cac2b1c3e8
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-29 13:10:54 -0700

    Create LoginViewName view and paste code

---
+++ RealAllSet/LoginViewName.swift
@@ -0,0 +1,80 @@
+//
+//  LoginViewName.swift
+//  AllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct LoginViewName: View {
+    
+    @Binding var name : String
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            ZStack {
+                
+                Color(Color("lightgreen"))
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(Color("vanilla").opacity(0.3))
+                
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(Color("vanilla"))
+                
+                VStack(alignment: .center) {
+                    
+                    Text("Welcome to All Set!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    Text("What's your name?")
+                        .font(.title2)
+                        .padding([.leading, .top, .trailing])
+                    
+                    TextField("First name", text: $name)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.black.opacity(0.05))
+                        .cornerRadius(15)
+                        .padding([.leading, .bottom, .trailing, .top])
+                    
+                    NavigationLink {
+                        
+                        LoginViewNewClasses()
+                        
+                    } label: {
+                        
+                        Text("Next")
+                            .padding()
+                            .foregroundColor(Color("vanilla"))
+                            .frame(width: 300, height: 50)
+                            .background(Color("lightgreen"))
+                            .cornerRadius(15)
+                        
+                    }//navlink
+                    
+                }//vstack
+                
+            }//zstack
+            
+        }//navstack
+        
+    }//body
+    
+}//struct
+
+
+#Preview {
+    
+    @Previewable @State var name = ""
+    LoginViewName(name: $name)
+    
+}//preview
---
+++ RealAllSet/LoginViewNewClasses.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewNewClasses.swift
+//  RealAllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct LoginViewNewClasses: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginViewNewClasses()
+}
