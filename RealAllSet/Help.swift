8019bf57d9009f4ea98bcef26f0209ba64b4436e
Lena Nichols <lena.k.nichols@gmail.com>
2025-07-30 12:00:25 -0700

    this is to save

--- RealAllSet/Help.swift
+++ RealAllSet/Help.swift
@@ -24,24 +24,60 @@ struct Help: View {
                         .cornerRadius(19)
                         .padding()
                     Spacer()
-                }
-                HStack {
-                    VStack{
-                        
-                        Text("Organize")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                            .background(Color("beigebrown")
-                            )
-                        
-                    }
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     
+                    HStack {
+                        VStack{
+                            
+                            Text("Being Organized")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            VStack(alignment: .leading) {
+                                Text("• Choose your area of focus")
+                                Text("• Use a to-do list")
+                                Text("• Set goals")
+                                Text("• Clean up physical clutter")
+                                Text("• Maintain a calender")
+
+
+                            }
+
+                            //Spacer()
+                        }
+                        .background(Color("beigebrown"))
+                        .cornerRadius(19)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+            
+                        
+                        VStack{
+                            
+                            Text("Reduce Stress")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            //Spacer()
+                        }
+                        .background(Color("beigebrown"))
+                        .cornerRadius(19)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+                    }//hsyack
                     
-                }
+                    Text ("Sources")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     
                         
+                        
+                        .background(Color("darkgreen"))
+                        .cornerRadius(19)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    
+                
+                }//vstack
             }
             
             
